92593fb5ce4dc142c372bda7eef54c31eb0df406
Nicholas Capel <nrjc2@cam.ac.uk>
2016-01-30 17:01:46 +0000

    With stock market training data

--- neural.py
+++ neural.py
@@ -1,20 +1,20 @@
 from fann2 import libfann
 
-connection_rate = 1
-learning_rate = 0.7
-num_input = 2
+connection_rate = 4
+learning_rate = 0.3
+num_input = 4
 num_hidden = 4
 num_output = 1
 
 desired_error = 0.0001
-max_iterations = 100000
-iterations_between_reports = 1000
+max_iterations = 100
+iterations_between_reports = 1
 
 ann = libfann.neural_net()
 ann.create_sparse_array(connection_rate, (num_input, num_hidden, num_output))
 ann.set_learning_rate(learning_rate)
 ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
 
-ann.train_on_file("xor.data", max_iterations, iterations_between_reports, desired_error)
+ann.train_on_file("EURUSDTESTTEST", max_iterations, iterations_between_reports, desired_error)
 
 ann.save("xor.net")
