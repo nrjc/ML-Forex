cfb7760642affebaa09412d69a6025fb0a7e63a0
Nicholas Capel <nrjc2@cam.ac.uk>
2016-01-30 17:34:02 +0000

    Neural network updated

--- neural.py
+++ neural.py
@@ -1,20 +1,20 @@
 from fann2 import libfann
 
-connection_rate = 4
-learning_rate = 0.3
+learning_rate = 0.7
+num_layers = 3
 num_input = 4
 num_hidden = 4
 num_output = 1
 
 desired_error = 0.0001
-max_iterations = 100
+max_iterations = 10
 iterations_between_reports = 1
 
 ann = libfann.neural_net()
-ann.create_sparse_array(connection_rate, (num_input, num_hidden, num_output))
+ann.create_standard_array((4,4,1))
 ann.set_learning_rate(learning_rate)
 ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
 
 ann.train_on_file("EURUSDTESTTEST", max_iterations, iterations_between_reports, desired_error)
 
-ann.save("xor.net")
+ann.save("new.net")
